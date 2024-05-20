4b0282f1c885ff6caba865821c8c12d75ea25098
nesirid <nasirid@code.edu.az>
2024-05-21 01:45:02 +0400

    first commit

---
+++ AspMVCHomeTask/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using AspMVCHomeTask.Data;
+using AspMVCHomeTask.Models;
+using AspMVCHomeTask.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AspMVCHomeTask.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _categoryService.GetAllWithProductCountAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            return View();
+        }
+    }
+}
